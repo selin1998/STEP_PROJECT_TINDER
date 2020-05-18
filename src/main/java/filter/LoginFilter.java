5a6c489b04c0971bb1a08e2fffec4a1524553c77
Selin H <selin150698@gmail.com>
2020-05-18 07:48:50 +0400

    Siddiga branch is completely merged to Selin

---
+++ src/main/java/filter/LoginFilter.java
@@ -0,0 +1,69 @@
+package filter;
+
+import dao.UserDAO;
+import db.DatabaseConnection;
+import entity.User;
+import org.eclipse.jetty.http.HttpMethod;
+import service.UserService;
+import util.TemplateEngine;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.HashMap;
+
+public class LoginFilter implements Filter {
+
+    private TemplateEngine engine = new TemplateEngine();
+    private UserDAO daoUser=new UserDAO();
+    private UserService usersService=new UserService(daoUser);
+
+    public LoginFilter(TemplateEngine engine) throws IOException, SQLException {
+        this.engine=engine;
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req;
+        if (request instanceof HttpServletRequest) {
+            req = (HttpServletRequest) request;
+        } else {
+            throw new IllegalArgumentException("ServletRequest should be instance of HttpServletRequest");
+        }
+
+        HashMap<String, Object> data = new HashMap<>();
+
+        if (HttpMethod.POST.name().equalsIgnoreCase(req.getMethod())) {
+            try {
+                String login = req.getParameter("Email");
+                String password = req.getParameter("Password");
+
+                if (!usersService.checkUser(login,password)) {
+                    throw new Exception("Incorrect login or password");
+                }
+                chain.doFilter(request, response);
+            } catch (Exception e) {
+                data.put("message", e.getMessage());
+                engine.render("error_message.ftl", data,(HttpServletResponse) response);
+            }
+        } else {
+            chain.doFilter(request, response);
+        }
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
--- src/main/java/servlet/RegisterServlet.java
+++ src/main/java/servlet/RegisterServlet.java
@@ -1,6 +1,64 @@
 package servlet;
 
+import dao.UserDAO;
+import entity.User;
+import service.CookiesService;
+import service.UserService;
+import util.TemplateEngine;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.HashMap;
 
 public class RegisterServlet extends HttpServlet {
+    private TemplateEngine engine;
+    private UserDAO daoUser = new UserDAO();
+    private UserService userService = new UserService(daoUser);
+
+    public RegisterServlet(TemplateEngine engine) throws SQLException {
+        this.engine = engine;
+    }
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HashMap<String, Object> data = new HashMap<>();
+        data.put("Name", "Name");
+        data.put("Surname", "Surname");
+        data.put("photoLink", "photolink");
+        data.put("Job", "Job");
+        data.put("Email", "Email");
+        data.put("Password", "Password");
+//        data.put("RepeatPassword","RepeatPassword");
+        data.put("message", "Please sign in");
+
+        engine.render("register.ftl", data, resp);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("Name");
+        String surname = req.getParameter("Surname");
+        String photoLink = req.getParameter("photolink");
+        String job = req.getParameter("Job");
+        String login = req.getParameter("Email");
+        String password = req.getParameter("Password");
+
+        User user = new User(login, password, name, surname, photoLink, job);
+        boolean result = userService.addUser(user);
+
+        CookiesService cookiesService = new CookiesService(req, resp);
+        if (result == true) {
+            cookiesService.addCookie(userService.getUserId(login, password));
+        } else {
+            new SQLException("Something wrong.User can not be added!");
+        }
+
+        resp.sendRedirect("/users");
+    }
 }
