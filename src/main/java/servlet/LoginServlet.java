029228e5b9b55d636c5fe3ad341eb2b4f7f6aa04
Selin H <selin150698@gmail.com>
2020-05-18 07:42:00 +0400

    merged login from Siddiga

--- src/main/java/servlet/LoginServlet.java
+++ src/main/java/servlet/LoginServlet.java
@@ -1,9 +1,57 @@
 package servlet;
 
-import org.eclipse.jetty.servlet.Source;
+import dao.DAO;
+import dao.UserDAO;
+import db.DatabaseConnection;
+import entity.User;
+import service.CookiesService;
+import service.UserService;
+import util.TemplateEngine;
 
-import javax.servlet.Servlet;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class LoginServlet extends HttpServlet {
+    private final TemplateEngine engine;
+    private UserDAO daoUser=new UserDAO();
+    private UserService usersService=new UserService(daoUser);
+    private CookiesService cookiesService;
+
+    public LoginServlet(TemplateEngine engine) throws SQLException {
+        this.engine = engine;
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        List<String> fields = new ArrayList<>();
+//        fields.add("Email");
+
+        HashMap<String, Object> data = new HashMap<>();
+//        data.put("fields", fields);
+        data.put("Email","Email");
+        data.put("message", "Please sign in");
+//        data.put("root", "/login");
+        engine.render("login.ftl", data, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String login = req.getParameter("Email");
+        String password = req.getParameter("Password");
+
+//        for add cookie
+//        User user=new User(login,password);
+       cookiesService = new CookiesService(req, resp);
+       cookiesService.addCookie(usersService.getUserId(login,password));
+
+        resp.sendRedirect("/users");
+    }
 }
