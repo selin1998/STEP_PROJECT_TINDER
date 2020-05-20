45ec9325cd36d6096007adf9083ec5e400f5ddf7
Selin H <selin150698@gmail.com>
2020-05-20 10:46:16 +0400

    heroku impl

--- src/main/java/TinderApp.java
+++ src/main/java/TinderApp.java
@@ -6,31 +6,28 @@ import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import servlet.*;
 import util.TemplateEngine;
-
-// import javax.servlet.DispatcherType;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import javax.servlet.DispatcherType;
 import java.util.EnumSet;
 
 public class TinderApp {
 
-  //  private static final EnumSet<DispatcherType> ft = EnumSet.of(DispatcherType.REQUEST);
+    private static final EnumSet<DispatcherType> ft = EnumSet.of(DispatcherType.REQUEST);
 
     public static void main(String[] args) throws Exception {
             Server server=new Server(HerokuEnv.port());
             TemplateEngine engine=new TemplateEngine();
             ServletContextHandler handler=new ServletContextHandler();
-         //   handler.addServlet(new ServletHolder(new MainPageServlet()),"/");
+      //      handler.addServlet(new ServletHolder(new MainPageServlet()),"/");
             handler.addServlet(new ServletHolder(new LoginServlet(engine)),"/login");
             handler.addServlet(new ServletHolder(new RegisterServlet(engine)),"/signup");
             handler.addServlet(new ServletHolder(new LogoutServlet()),"/logout");
             handler.addServlet(new ServletHolder(new UserServlet(engine)),"/users");
             handler.addServlet(new ServletHolder(new LikedServlet(engine)),"/liked");
             handler.addServlet(new ServletHolder(new MessageServlet(engine)),"/messages/*");
-        //    handler.addFilter(new FilterHolder(new LoginFilter(engine)), "/login/*", ft);
-        //    handler.addFilter(HttpFilter.class,"/users",ft);
-       //     handler.addFilter(HttpFilter.class,"/liked",ft);
-       //     handler.addFilter(HttpFilter.class,"/messages/*",ft);
+            handler.addFilter(new FilterHolder(new LoginFilter(engine)), "/login/*", ft);
+            handler.addFilter(HttpFilter.class,"/users",ft);
+           handler.addFilter(HttpFilter.class,"/liked",ft);
+           handler.addFilter(HttpFilter.class,"/messages/*",ft);
 
 
             server.setHandler(handler);
